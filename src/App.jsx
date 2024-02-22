840bc8a814e227a243cf87bc4279fcc449169288
KJR020 <johnjiro1114@gmail.com>
2024-02-23 08:53:08 +0900

    Refactor App Component to use allow function syntax and update JSX component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import "./styles.css";
-
-export default function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <h1>こんにちは！</h1>
+      <p>paiapi</p>
+    </>
   );
-}
+};
