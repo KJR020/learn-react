1784890b04ae8d2d2d5174822df56ab5a6f5eb49
KJR020 <johnjiro1114@gmail.com>
2024-02-23 07:10:45 +0900

    Update index.jsx with new App component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>paiapi</p>
+    </>
+  );
+};
 
 root.render(
   <StrictMode>
