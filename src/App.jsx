8864a9ba4afa85821ffee8bbb83e87650f286926
KJR020 <johnjiro1114@gmail.com>
2024-02-23 09:42:18 +0900

    Add button and style changes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 export const App = () => {
+  const onClickButton = () => {
+    alert('Clicked')
+  };
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20px", // font-sizeではなく、fontSizeとする必要がある。JSでは-が使えないらしい
+  }
+
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>paiapi</p>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>How are you?</p>
+      <button onClick={onClickButton}>Button</button>
     </>
   );
 };
