8d095cf93a1d70fd5153801e8f5719c656c709f6
KJR020 <johnjiro1114@gmail.com>
2024-02-24 14:50:22 +0900

    Feature: Implement count up functionality on button click in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { ColorfulMessage } from "./components/ColofulMessage";
+import { useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => {
-    alert('Clicked')
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1);
+    setNum((prev) => prev + 1);
   };
   const contentStyle = {
     color: "blue",
@@ -14,7 +17,8 @@ export const App = () => {
       <h1 style={{ color: "red" }}>Hello, World!</h1>
       <ColorfulMessage color="skyblue">How are you?</ColorfulMessage> 
       <ColorfulMessage color="green">I'm fine.</ColorfulMessage>
-      <button onClick={onClickButton}>Button</button>
+      <button onClick={onClickCountUp}>Count Up Button</button>
+      <p>{num}</p>
     </>
   );
 };
